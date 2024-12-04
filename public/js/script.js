001304486c4eec5286a7c9b66218e15d47dbca0e
Yogesh Pote <yogpote035@gmail.com>
2024-12-04 22:32:01 +0530

    fix tax button

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,17 @@
       }, false)
     })
   })()
+
+
+
+  let taxSwitchBtn = document.getElementById("flexSwitchCheckDefault");
+  taxSwitchBtn.addEventListener("click", () => {
+      let taxInfo = document.getElementsByClassName("tax-info");
+      for (info of taxInfo) {
+          if (info.style.display != 'inline') {
+              info.style.display = 'inline';
+          } else {
+              info.style.display = 'none'
+          }
+      }
+  })
