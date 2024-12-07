72dbb4bd582728f58577e657f6d83735c147a3a2
Yogesh Pote <yogpote035@gmail.com>
2024-12-07 18:17:36 +0530

    Revert "cross button for nav"
    This reverts commit 34a8bf84233947834d25204d8d1c8a636508c3f8.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,18 +1,14 @@
 let navDiv = document.querySelector(".box-nav");
 
 let btn = document.querySelector(".navbar-toggler");
-let crossAndBar = document.querySelector(".CrossAndBar i");
 
 
 btn.addEventListener("click", () => {
-    if (navDiv.style.display === 'none' && crossAndBar.classList.contains("fa-bars-staggered")) {
-        crossAndBar.classList.remove("fa-bars-staggered");
-        crossAndBar.classList.add("fa-xmark");
+    if (navDiv.style.display === 'none') {
         navDiv.style.display = 'block'
     }
     else {
         navDiv.style.display = 'none'
-        crossAndBar.classList.remove("fa-xmark");
-        crossAndBar.classList.add("fa-bars-staggered");
+
     }
 });
