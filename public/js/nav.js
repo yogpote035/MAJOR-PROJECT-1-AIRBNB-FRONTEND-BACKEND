34a8bf84233947834d25204d8d1c8a636508c3f8
Yogesh Pote <yogpote035@gmail.com>
2024-12-07 18:09:40 +0530

    cross button for nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,14 +1,18 @@
 let navDiv = document.querySelector(".box-nav");
 
 let btn = document.querySelector(".navbar-toggler");
+let crossAndBar = document.querySelector(".CrossAndBar i");
 
 
 btn.addEventListener("click", () => {
-    if (navDiv.style.display === 'none') {
+    if (navDiv.style.display === 'none' && crossAndBar.classList.contains("fa-bars-staggered")) {
+        crossAndBar.classList.remove("fa-bars-staggered");
+        crossAndBar.classList.add("fa-xmark");
         navDiv.style.display = 'block'
     }
     else {
         navDiv.style.display = 'none'
-
+        crossAndBar.classList.remove("fa-xmark");
+        crossAndBar.classList.add("fa-bars-staggered");
     }
 });
