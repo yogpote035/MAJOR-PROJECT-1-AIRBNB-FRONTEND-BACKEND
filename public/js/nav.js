0505472ac64fd867df4f1a6c5ab2663db764fee5
Yogesh Pote <yogpote035@gmail.com>
2024-12-09 18:11:05 +0530

    fix nav tax using font awsome icon custome

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,11 +2,11 @@ let navDiv = document.querySelector(".box-nav");
 let btn = document.querySelector(".navbar-toggler");
 let crossAndBar = document.querySelector(".CrossAndBar i");
 
+// nav bar and cross button
+
 btn.addEventListener("click", () => {
-  // Toggle visibility using CSS classes
   navDiv.classList.toggle("show");
 
-  // Toggle icon classes
   if (crossAndBar.classList.contains("fa-bars-staggered")) {
     crossAndBar.classList.remove("fa-bars-staggered");
     crossAndBar.classList.add("fa-xmark");
@@ -15,3 +15,43 @@ btn.addEventListener("click", () => {
     crossAndBar.classList.add("fa-bars-staggered");
   }
 });
+
+
+// tax button
+
+let taxSwitchBtns = document.querySelectorAll(".tax-button i");
+
+taxSwitchBtns.forEach((taxSwitchBtn) => {
+  taxSwitchBtn.addEventListener("click", () => {
+    let taxInfo = document.querySelectorAll(".tax-info");
+
+    // Toggle the icon classes
+    if (taxSwitchBtn.classList.contains("fa-toggle-off")) {
+      taxSwitchBtn.classList.remove("fa-toggle-off");
+      taxSwitchBtn.classList.add("fa-toggle-on");
+    } else {
+      taxSwitchBtn.classList.remove("fa-toggle-on");
+      taxSwitchBtn.classList.add("fa-toggle-off");
+    }
+
+    // Toggle the visibility of .tax-info elements
+    taxInfo.forEach((info) => {
+      info.style.display = info.style.display !== "inline" ? "inline" : "none";
+    });
+  });
+});
+
+
+// carousel nav category icons
+
+function scrollFilters(scrollValue) {
+  const filtersContainer = document.querySelector(".filters");
+
+  if (filtersContainer) {
+    // Scroll the filters container
+    filtersContainer.scrollBy({
+      left: scrollValue,
+      behavior: "smooth"
+    });
+  }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -17,32 +17,3 @@
     })
   })()
 
-
-
-  let taxSwitchBtn = document.getElementById("flexSwitchCheckDefault");
-  taxSwitchBtn.addEventListener("click", () => {
-      let taxInfo = document.getElementsByClassName("tax-info");
-      for (info of taxInfo) {
-          if (info.style.display != 'inline') {
-              info.style.display = 'inline';
-          } else {
-              info.style.display = 'none'
-          }
-      }
-  })
-
-
-  
-
-
-  function scrollFilters(scrollValue) {
-    const filtersContainer = document.querySelector(".filters");
-
-    if (filtersContainer) {
-        // Scroll the filters container
-        filtersContainer.scrollBy({
-            left: scrollValue,
-            behavior: "smooth"
-        });
-    }
-}
